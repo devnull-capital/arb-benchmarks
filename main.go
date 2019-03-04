40d09e0973a0887c9a50f722f19785284c60568c
adam <ahanna@alumni.mines.edu>
2019-03-03 19:59:47 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,127 @@
+package arbitrage
+
+type rate struct {
+	from, to, exchange string
+	rate, vol          float64
+}
+
+func arbFromRates(rates []*rate, depth uint) [][][]*rate {
+	return arbFromCombos(combosFromRates(rates, depth))
+}
+
+func arbFromCombos(combos [][][]*rate) [][][]*rate {
+	ret := make([][][]*rate, len(combos))
+	for i := range combos {
+		for j := range combos[i] {
+			if isArb(combos[i][j]) && !isDupe(ret[i], combos[i][j]) {
+				ret[i] = append(ret[i], combos[i][j])
+			}
+		}
+	}
+
+	return ret
+}
+
+func isDupe(list [][]*rate, arb []*rate) bool {
+	if list == nil || len(list) == 0 {
+		return false
+	}
+
+	m := make(map[*rate]struct{})
+	for i := range arb {
+		m[arb[i]] = struct{}{}
+	}
+
+	var (
+		count int
+		ok    bool
+	)
+	for i := range list {
+		count = 0
+		for j := range list[i] {
+			if _, ok = m[list[i][j]]; ok {
+				count++
+			}
+		}
+
+		if count == len(list[i]) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isArb(list []*rate) bool {
+	if len(list) < 2 {
+		return false
+	}
+
+	if list[0].from != list[len(list)-1].to {
+		return false
+	}
+
+	prod := list[0].rate
+	for i := 1; i < len(list); i++ {
+		if list[i-1].to != list[i].from {
+			return false
+		}
+
+		prod *= list[i].rate
+	}
+
+	return prod > 1.0
+}
+
+func combosFromRates(rates []*rate, depth uint) [][][]*rate {
+	// note: no safety checks
+	//if rates == nil || len(rates) < 2 {
+	//return nil, errors.New("not enough rates")
+	//}
+
+	//if depth < 1 {
+	//return nil, errors.New("min depth is 1")
+	//}
+
+	ret := make([][][]*rate, int(depth))
+	ret[0] = buildBase(rates)
+	for i := 1; i < int(depth); i++ {
+		for j := range ret[i-1] {
+			for k := range rates {
+				if ret[i-1][j][len(ret[i-1][j])-1].to == rates[k].from && !isRateInList(ret[i-1][j], rates[k]) && !isListClosing(ret[i-1][j]) {
+					ret[i] = append(ret[i], append(ret[i-1][j], rates[k]))
+				}
+			}
+		}
+	}
+
+	return ret
+}
+
+func isListClosing(list []*rate) bool {
+	return list[0].from == list[len(list)-1].to
+}
+
+func buildBase(rates []*rate) [][]*rate {
+	var ret [][]*rate
+	for i := range rates {
+		for j := i + 1; j < len(rates); j++ {
+			if rates[i].to == rates[j].from {
+				ret = append(ret, []*rate{rates[i], rates[j]})
+			}
+		}
+	}
+
+	return ret
+}
+
+func isRateInList(list []*rate, r *rate) bool {
+	// note: not doing nil checks for speed reasons...
+	for idx := range list {
+		if list[idx] == r {
+			return true
+		}
+	}
+
+	return false
+}
---
+++ main_test.go
@@ -0,0 +1,424 @@
+package arbitrage
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsRateInList(t *testing.T) {
+	r := &rate{
+		from:     "foo",
+		to:       "bar",
+		exchange: "baz",
+		vol:      1.0,
+		rate:     1.0,
+	}
+
+	for i, tt := range []struct {
+		list     []*rate
+		r        *rate
+		expected bool
+	}{
+		{
+			list: []*rate{
+				&rate{
+					from:     "foo",
+					to:       "bar",
+					exchange: "baz",
+					vol:      1.0,
+					rate:     1.0,
+				},
+				&rate{
+					from:     "foo",
+					to:       "bar",
+					exchange: "baz",
+					vol:      1.0,
+					rate:     1.0,
+				},
+			},
+			r: &rate{
+				from:     "foo",
+				to:       "bar",
+				exchange: "baz",
+				vol:      1.0,
+				rate:     1.0,
+			},
+			expected: false,
+		},
+		{
+			list: []*rate{
+				&rate{
+					from:     "foo",
+					to:       "bar",
+					exchange: "baz",
+					vol:      1.0,
+					rate:     1.0,
+				},
+				&rate{
+					from:     "foo",
+					to:       "bar",
+					exchange: "baz",
+					vol:      1.0,
+					rate:     1.0,
+				},
+				r,
+			},
+			r:        r,
+			expected: true,
+		},
+	} {
+		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
+			if isRateInList(tt.list, tt.r) != tt.expected {
+				t.Errorf("expected %v from rate %v in list %v", tt.expected, tt.r, tt.list)
+			}
+		})
+	}
+}
+
+func TestBuildBase(t *testing.T) {
+	t.Run("should not build any when the to's and from's don't match", func(t *testing.T) {
+		rates := []*rate{
+			&rate{},
+			&rate{
+				to:   "foo",
+				from: "foo",
+			},
+		}
+		r := buildBase(rates)
+		if len(r) != 0 {
+			t.Errorf("expected zero length result, received %d for %v", len(r), r)
+		}
+	})
+	t.Run("should build on proper input", func(t *testing.T) {
+		rates := []*rate{
+			&rate{
+				from: "a",
+				to:   "b",
+			},
+			&rate{
+				from: "b",
+				to:   "c",
+			},
+			&rate{
+				from: "c",
+				to:   "d",
+			},
+			&rate{
+				from: "y",
+				to:   "z",
+			},
+		}
+		r := buildBase(rates)
+		if len(r) != 2 || len(r[0]) != 2 || len(r[1]) != 2 {
+			t.Errorf("expected two length result with three sub-length, received %d for %v", len(r), r)
+		}
+	})
+}
+
+func TestCombosFromRates(t *testing.T) {
+	t.Run("should build on proper input", func(t *testing.T) {
+		rates := []*rate{
+			&rate{
+				from: "a",
+				to:   "b",
+			},
+			&rate{
+				from: "b",
+				to:   "c",
+			},
+			&rate{
+				from: "c",
+				to:   "d",
+			},
+			&rate{
+				from: "y",
+				to:   "z",
+			},
+		}
+		r := combosFromRates(rates, 4)
+		if len(r) != 4 || len(r[0]) != 2 || len(r[0][0]) != 2 || len(r[0][1]) != 2 || len(r[1]) != 1 || len(r[1][0]) != 3 {
+			t.Errorf("expected two length result with three sub-length, received %d for %v", len(r), r)
+		}
+	})
+}
+
+func TestIsArb(t *testing.T) {
+	for i, tt := range []struct {
+		list     []*rate
+		expected bool
+	}{
+		{
+			list: []*rate{
+				&rate{},
+			},
+			expected: false,
+		},
+		{
+			list: []*rate{
+				&rate{
+					from: "a",
+					to:   "b",
+				},
+				&rate{
+					from: "y",
+					to:   "z",
+				},
+			},
+			expected: false,
+		},
+		{
+			list: []*rate{
+				&rate{
+					from: "a",
+					to:   "b",
+				},
+				&rate{
+					from: "b",
+					to:   "c",
+				},
+			},
+			expected: false,
+		},
+		{
+			list: []*rate{
+				&rate{
+					from: "a",
+					to:   "b",
+					rate: 2.0,
+				},
+				&rate{
+					from: "b",
+					to:   "c",
+					rate: 2.0,
+				},
+			},
+			expected: false,
+		},
+		{
+			list: []*rate{
+				&rate{
+					from: "a",
+					to:   "b",
+					rate: 2.0,
+				},
+				&rate{
+					from: "b",
+					to:   "c",
+					rate: 2.0,
+				},
+				&rate{
+					from: "c",
+					to:   "a",
+					rate: 2.0,
+				},
+			},
+			expected: true,
+		},
+		{
+			list: []*rate{
+				&rate{
+					from: "a",
+					to:   "b",
+					rate: 1.0,
+				},
+				&rate{
+					from: "b",
+					to:   "c",
+					rate: 1.0,
+				},
+				&rate{
+					from: "c",
+					to:   "a",
+					rate: 1.0,
+				},
+			},
+			expected: false,
+		},
+	} {
+		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
+			if isArb(tt.list) != tt.expected {
+				t.Errorf("expected %v from list %v", tt.expected, tt.list)
+			}
+		})
+	}
+}
+
+func TestArbFromCombos(t *testing.T) {
+	t.Run("good rates should produce arb", func(t *testing.T) {
+		rates := []*rate{
+			&rate{
+				from: "a",
+				to:   "b",
+				rate: 2.0,
+			},
+			&rate{
+				from: "b",
+				to:   "c",
+				rate: 2.0,
+			},
+			&rate{
+				from: "c",
+				to:   "a",
+				rate: 2.0,
+			},
+			&rate{
+				from: "y",
+				to:   "z",
+				rate: 2.0,
+			},
+		}
+		arb := arbFromCombos(combosFromRates(rates, 4))
+		if len(arb) != 4 || len(arb[0]) != 0 || len(arb[1]) != 1 || len(arb[1][0]) != 3 || len(arb[2]) != 0 || len(arb[3]) != 0 {
+			t.Errorf("returned bad arb\n%v", arb)
+		}
+	})
+}
+
+func TestIsDupe(t *testing.T) {
+	r1 := &rate{}
+	r2 := &rate{}
+	r3 := &rate{}
+	arb := []*rate{r1, r2, r3}
+
+	for i, tt := range []struct {
+		list     [][]*rate
+		arb      []*rate
+		expected bool
+	}{
+		{
+			list: [][]*rate{
+				[]*rate{
+					&rate{},
+					&rate{},
+				},
+				arb,
+			},
+			arb:      arb,
+			expected: true,
+		},
+		{
+			list: [][]*rate{
+				[]*rate{
+					&rate{},
+					&rate{},
+				},
+				[]*rate{
+					&rate{},
+					&rate{},
+				},
+			},
+			arb:      arb,
+			expected: false,
+		},
+	} {
+		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
+			if isDupe(tt.list, tt.arb) != tt.expected {
+				t.Errorf("expected %v with list %v and arb %v", tt.expected, tt.list, tt.arb)
+			}
+		})
+	}
+}
+
+func BenchmarkArbFromRates(b *testing.B) {
+	rates := []*rate{
+		&rate{
+			from: "a",
+			to:   "b",
+			rate: 2.0,
+		},
+		&rate{
+			from: "b",
+			to:   "c",
+			rate: 2.0,
+		},
+		&rate{
+			from: "c",
+			to:   "d",
+			rate: 2.0,
+		},
+		&rate{
+			from: "d",
+			to:   "e",
+			rate: 2.0,
+		},
+		&rate{
+			from: "b",
+			to:   "a",
+			rate: 2.0,
+		},
+		&rate{
+			from: "c",
+			to:   "a",
+			rate: 2.0,
+		},
+		&rate{
+			from: "d",
+			to:   "a",
+			rate: 2.0,
+		},
+		&rate{
+			from: "c",
+			to:   "b",
+			rate: 2.0,
+		},
+		&rate{
+			from: "d",
+			to:   "b",
+			rate: 2.0,
+		},
+		&rate{
+			from: "d",
+			to:   "c",
+			rate: 2.0,
+		},
+	}
+
+	for i := 0; i < b.N; i++ {
+		_ = arbFromRates(rates, 10)
+	}
+}
+
+func TestIsListClosing(t *testing.T) {
+	for i, tt := range []struct {
+		list     []*rate
+		expected bool
+	}{
+		{
+			list: []*rate{
+				&rate{
+					from: "a",
+					to:   "b",
+				},
+				&rate{
+					from: "b",
+					to:   "a",
+				},
+			},
+			expected: true,
+		},
+		{
+			list: []*rate{
+				&rate{
+					from: "a",
+					to:   "b",
+				},
+				&rate{
+					from: "b",
+					to:   "c",
+				},
+				&rate{
+					from: "y",
+					to:   "z",
+				},
+			},
+			expected: false,
+		},
+	} {
+		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
+			if tt.expected != isListClosing(tt.list) {
+				t.Errorf("expected %v from list %v", tt.expected, tt.list)
+			}
+		})
+	}
+
+}
