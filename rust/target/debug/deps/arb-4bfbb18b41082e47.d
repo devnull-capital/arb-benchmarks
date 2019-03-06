a1a9d15911e88e24d48a203a7a906904d8bc91a4
adam <ahanna@alumni.mines.edu>
2019-03-06 10:41:17 -0800

    updated gitignore

--- rust/target/debug/deps/arb-4bfbb18b41082e47.d
+++
@@ -1,5 +0,0 @@
-/home/adam/go/src/github.com/adam-hanna/arb-test/rust/target/debug/deps/libarb-4bfbb18b41082e47.rlib: src/lib.rs
-
-/home/adam/go/src/github.com/adam-hanna/arb-test/rust/target/debug/deps/arb-4bfbb18b41082e47.d: src/lib.rs
-
-src/lib.rs:
--- rust/target/debug/deps/arb-d026e17ef942ece0.d
+++
@@ -1,5 +0,0 @@
-/home/adam/go/src/github.com/adam-hanna/arb-test/rust/target/debug/deps/arb-d026e17ef942ece0: src/lib.rs
-
-/home/adam/go/src/github.com/adam-hanna/arb-test/rust/target/debug/deps/arb-d026e17ef942ece0.d: src/lib.rs
-
-src/lib.rs:
